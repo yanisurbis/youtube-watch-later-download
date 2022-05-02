4cb91a890a06906b9e5f7ec03f4720fe4d45f298
Yanis Urbis <yanis.urbis@gmail.com>
2022-05-02 14:57:27 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-function scrollToTheBottom() {
-	document.querySelector('#continuations')
-  .scrollIntoView({
-    behavior: 'smooth'
-  });
-}
-
-function debounce(func, timeout = 5000){
-  let timer;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => { func.apply(this, args); }, timeout);
-  };
-}
-
-function getData() {
+function saveVideosInfoToClipboard() {
 	const videoElms = document.querySelectorAll("#container.style-scope.ytd-playlist-video-renderer")
 	let videos = []
 	for (let elm of videoElms) {
@@ -25,19 +10,37 @@ function getData() {
 			channel: { link: channel.href, name: channel.innerText },
 			title: video.title,
 			link: video.href,
-			thumbnail: thumbnail.src,
+			thumbnailSrc: thumbnail.src,
 			duration: duration.innerText
 		})
 	}
 	copy(videos)
-	console.log(`We copied ${videos.length} videos to your clipboard`)
+	console.log(`We copied ${videos.length} videos to buffer`)
 	window.videos = videos
 	return videos
 }
 
-const observer = new MutationObserver(debounce(function(mutationsList, observer) {
-	getData()
-	setTimeout(scrollToTheBottom, 5000)
-}));
+function scrollToTheVeryBottomOfThePlaylist() {
+	function scrollToTheBottom() {
+		document.querySelector('#continuations')
+	  .scrollIntoView({
+	    behavior: 'smooth'
+	  });
+	}
 
-observer.observe(document.querySelector("#contents"), { attributes: true, childList: true, subtree: true });
+	function debounce(func, timeout = 5000){
+	  let timer;
+	  return (...args) => {
+	    clearTimeout(timer);
+	    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+	  };
+	}
+
+	const observer = new MutationObserver(debounce(function(mutationsList, observer) {
+		setTimeout(scrollToTheBottom, 1000)
+	}));
+
+	observer.observe(document.querySelector("#contents"), { attributes: true, childList: true, subtree: true });
+
+	scrollToTheBottom()
+}
