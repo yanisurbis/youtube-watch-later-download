4b1920af0eba2cfebce7c613d26a3619ddbfd1b0
Yanis Urbis <yanis.urbis@gmail.com>
2022-05-02 17:47:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,61 @@
-function saveVideosInfoToClipboard() {
-	const videoElms = document.querySelectorAll("#container.style-scope.ytd-playlist-video-renderer")
-	let videos = []
-	for (let elm of videoElms) {
-		const channel = elm.querySelector(".ytd-channel-name a")
-		const video = elm.querySelector("a#video-title")
-		const thumbnail = elm.querySelector("img")
-		const duration = elm.querySelector("ytd-thumbnail-overlay-time-status-renderer")
-		videos.push({
-			channel: { link: channel.href, name: channel.innerText },
-			title: video.title,
-			link: video.href,
-			thumbnailSrc: thumbnail.src,
-			duration: duration.innerText
-		})
-	}
-	copy(videos)
-	console.log(`We copied ${videos.length} videos to your clipboard`)
-	window.videos = videos
-	return videos
-}
+(function scrollToTheVeryBottomOfThePlaylistAndSave() {
+	const DEFAULT_TIMEOUT = 5000
 
-function scrollToTheVeryBottomOfThePlaylist() {
+	function retrieveVideosFromThePage() {
+		const videoElms = document.querySelectorAll("#container.style-scope.ytd-playlist-video-renderer")
+		let videos = []
+		for (let elm of videoElms) {
+			const channel = elm.querySelector(".ytd-channel-name a")
+			const video = elm.querySelector("a#video-title")
+			const thumbnail = elm.querySelector("img")
+			const duration = elm.querySelector("#text.ytd-thumbnail-overlay-time-status-renderer")
+			videos.push({
+				channel: { link: channel?.href, name: channel?.innerText },
+				title: video?.title,
+				link: video?.href,
+				thumbnailSrc: thumbnail?.src,
+				duration: duration?.innerText
+			})
+		}
+		return videos
+	}
+
+	function saveVideos(videos) {
+		copy(videos)
+		console.log(`We put ${videos.length} videos to your clipboard`)
+		window.videos = videos
+		console.log(`We put ${videos.length} videos into window.videos variable`)
+		try {
+			const playlistTitle = document.querySelector("#title-form")?.innerText ?? `title-${Math.floor(Math.random() * 1000)}`
+			const key = `${playlistTitle}-videos`
+			localStorage.setItem(key, JSON.stringify(videos));
+			console.log(`We put ${videos.length} videos into the local storage under the ${key} key`)
+		} catch (e) {
+			console.log("Failed to put videos to the local storage")
+		}
+	}
+
 	function scrollToTheBottom() {
 		document.querySelector('#continuations')
-	  .scrollIntoView({
-	    behavior: 'smooth'
-	  });
+			  .scrollIntoView({
+			    behavior: 'smooth'
+			  });
 	}
 
-	function debounce(func, timeout = 5000){
-	  let timer;
+	function debounceWithFallback(func, funcFallback, timeout = DEFAULT_TIMEOUT){
+	  let timer = setTimeout(() => { funcFallback(); }, timeout);
 	  return (...args) => {
 	    clearTimeout(timer);
-	    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+	    timer = setTimeout(() => { func(args); }, timeout);
 	  };
 	}
 
-	const observer = new MutationObserver(debounce(function(mutationsList, observer) {
-		setTimeout(scrollToTheBottom, 1000)
+	const observer = new MutationObserver(debounceWithFallback(scrollToTheBottom, () => {
+		saveVideos(retrieveVideosFromThePage())
 	}));
 
 	observer.observe(document.querySelector("#contents"), { attributes: true, childList: true, subtree: true });
 
 	scrollToTheBottom()
-}
+	console.log(`Please wait for ${DEFAULT_TIMEOUT / 1000} seconds...`)
+})()
